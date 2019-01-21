5ba0af75129ab127f31c089afb55d2d5d9f05fe3
lixiang <dreaming_lee@outlook.com>
2019-01-21 14:30:12 +0800

    init

---
+++ src/main/antlr4/OddlGrammar.g4
@@ -0,0 +1,183 @@
+grammar OddlGrammar;
+
+root
+    : oddlStatements? EOF
+    ;
+
+oddlStatements
+    : (oddlStatement SEMI | emptyStatement)+
+    ;
+
+oddlStatement
+    : createSourceClass
+    | createDeputyClass
+    | dropClass
+    | insertIntoClass
+    | deleteFromClass
+    | simpleQuery
+    | crossClassQuery
+    | updateObject
+    ;
+
+createSourceClass
+    : CREATE CLASS className createDefinitions
+    ;
+
+createDefinitions
+    : '(' createDefinition (',' createDefinition)* ')'
+    ;
+
+createDefinition
+    : ID dataType;
+
+createDeputyClass
+    : CREATE SELECTDEPUTY className
+      SELECT expression AS dAttr (',' expression AS dAttr)* 
+      FROM sClassName
+      (WHERE expression)?
+    ;
+
+dropClass
+    : DROP CLASS className;
+
+insertIntoClass
+    : INSERT INTO className VALUES valueList;
+
+valueList
+    : '(' value (',' value)* ')'
+    ;
+
+value
+    : STRING_LITERAL
+    | (SIGNED_DECIMAL | DECIMAL)
+    | (SIGNED_REAL | REAL)
+    | (TRUE | FALSE)
+    ;
+
+deleteFromClass
+    : DELETE FROM className (WHERE expression)?;
+
+simpleQuery
+    : SELECT attrList FROM className (WHERE expression)?;
+
+crossClassQuery
+    : SELECT className (CROSS className)* DOT attrName 
+      FROM className
+      (WHERE expression)?
+    ;
+
+updateObject
+    : UPDATE className SET attrName '=' value
+      WHERE expression
+    ;
+
+
+expression
+    : value
+    | attrName 
+    | expression op=('*' | '/' | '%') expression
+    | expression op=('+' | '-') expression
+    | expression op=('<' | '<=' | '>' | '>=') expression
+    | expression op=('=' | '==' | '!=' | '<>' ) expression
+    | expression AND expression
+    | expression OR  expression
+    | '(' expression ')'
+    ;
+
+emptyStatement
+    : SEMI
+    ;
+
+sClassName: className;
+sAttr: attrName;
+dAttr: attrName;
+className: ID;
+attrName:  ID;
+attrList
+    : attrName (',' attrName)
+    ;
+dataType
+    : CHAR ('(' DECIMAL ')')?
+    | typeName=(INT | FLOAT | BOOLEAN)
+    ;
+
+// Keywords
+AS : A S;
+CLASS:  C L A S S;
+CREATE : C R E A T E;
+CROSS : C R O S S;
+DELETE : D E L E T E;
+DROP : D R O P;
+FROM : F R O M;
+INSERT : I N S E R T;
+INTO : I N T O;
+SELECT : S E L E C T;
+SELECTDEPUTY: S E L E C T D E P U T Y;
+UPDATE : U P D A T E;
+VALUES : V A L U E S;
+SET : S E T;
+WHERE : W H E R E;
+
+BOOLEAN: B O O L E A N;
+CHAR:    C H A R;
+INT:     I N T;
+LONG:    L O N G;
+FLOAT:   F L O A T;
+
+TRUE: T R U E;
+FALSE: F A L S E;
+AND: A N D ;
+OR:  O R;
+NOT: N O T;
+
+// Constructors symbols
+DOT:                '.';
+LR_BRACKET:         '(';
+RR_BRACKET:         ')';
+COMMA:              ',';
+SEMI:               ';';
+EQUAL_SYMBOL:       '=';
+GREATER_SYMBOL:     '>';
+LESS_SYMBOL:        '<';
+
+SPACE: [ \t\r\n]+    -> channel(HIDDEN);
+
+DECIMAL: DEC_DIGIT+;
+REAL:    DEC_DIGIT+ '.' DEC_DIGIT+;
+STRING_LITERAL
+ : '\'' ( ~'\'' | '\'\'' )* '\''
+ ;
+
+SIGNED_DECIMAL: ('+' | '-')? DECIMAL;
+SIGNED_REAL: ('+' | '-')? REAL;
+
+ID : [a-zA-Z_] [a-zA-Z0-9_]* ;
+
+fragment DEC_DIGIT:  [0-9];
+
+fragment A : [aA];
+fragment B : [bB];
+fragment C : [cC];
+fragment D : [dD];
+fragment E : [eE];
+fragment F : [fF];
+fragment G : [gG];
+fragment H : [hH];
+fragment I : [iI];
+fragment J : [jJ];
+fragment K : [kK];
+fragment L : [lL];
+fragment M : [mM];
+fragment N : [nN];
+fragment O : [oO];
+fragment P : [pP];
+fragment Q : [qQ];
+fragment R : [rR];
+fragment S : [sS];
+fragment T : [tT];
+fragment U : [uU];
+fragment V : [vV];
+fragment W : [wW];
+fragment X : [xX];
+fragment Y : [yY];
+fragment Z : [zZ];
---
+++ src/main/java/com/nanimono/simpleoddb/executor/ExecutorListener.java
@@ -0,0 +1,191 @@
+package com.nanimono.simpleoddb.executor;
+
+import com.nanimono.simpleoddb.Catalog;
+import com.nanimono.simpleoddb.object.Type;
+import com.nanimono.simpleoddb.object.TypeEnum;
+import com.nanimono.simpleoddb.executor.antlr4.OddlGrammarBaseListener;
+import com.nanimono.simpleoddb.executor.antlr4.OddlGrammarParser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+public class ExecutorListener extends OddlGrammarBaseListener {
+
+    private Catalog catalog;
+
+    /**
+     * 分析expression所使用的成员变量
+     */
+    private ExprTreeNode current;
+    private Stack<ExprTreeNode> treeNodeStack;
+    private Stack<ExprTreeNode> rootNodeStack;
+
+    public ExecutorListener(Catalog catalog) {
+        this.catalog = catalog;
+        current = null;
+        treeNodeStack = null;
+        rootNodeStack = null;
+    }
+
+    @Override
+    public void enterExpression(OddlGrammarParser.ExpressionContext ctx) {
+        if (ctx.LR_BRACKET() != null) return;
+        current = new ExprTreeNode();
+        if (ctx.value() != null) {
+            OddlGrammarParser.ValueContext value = ctx.value();
+            if (value.STRING_LITERAL() != null) {
+                current.setType(ExprTreeNode.ExprTreeNodeType.CONST_CHAR);
+                current.setData(value.getText());
+            }
+            else if (value.DECIMAL() != null || value.SIGNED_DECIMAL() != null) {
+                current.setType(ExprTreeNode.ExprTreeNodeType.CONST_INT);
+                current.setData(Integer.parseInt(value.getText()));
+            }
+            else if (value.REAL() != null || value.SIGNED_REAL() != null) {
+                current.setType(ExprTreeNode.ExprTreeNodeType.CONST_FLOAT);
+                current.setData(Float.parseFloat(value.getText()));
+            }
+            else if (value.TRUE() != null || value.FALSE() != null) {
+                current.setType(ExprTreeNode.ExprTreeNodeType.CONST_BOOL);
+                current.setData(Boolean.parseBoolean(value.getText()));
+            }
+
+        }
+        else if (ctx.attrName() != null) {
+            current.setType(ExprTreeNode.ExprTreeNodeType.VAR);
+            current.setData(ctx.attrName().getText());
+        }
+        else {
+            current.setType(ExprTreeNode.ExprTreeNodeType.OP);
+            current.setData(ctx.children.get(1).getText());
+        }
+        treeNodeStack.push(current);
+        System.out.println(ctx.getText());
+    }
+
+    @Override
+    public void exitExpression(OddlGrammarParser.ExpressionContext ctx) {
+        if (ctx.LR_BRACKET() != null) return;
+        current = treeNodeStack.pop();
+        if (!treeNodeStack.isEmpty()) {
+            ExprTreeNode father = treeNodeStack.peek();
+            if (father.getLchild() == null) father.setLchild(current);
+            else if (father.getRchild() == null) father.setRchild(current);
+            else throw new UnsupportedOperationException("Expression tree build failed.");
+        }
+        if (treeNodeStack.isEmpty()) rootNodeStack.push(current);
+        System.out.println(ctx.getText());
+    }
+
+    @Override
+    public void exitCreateSourceClass(OddlGrammarParser.CreateSourceClassContext ctx) {
+        String className = ctx.className().ID().getText();
+        List<OddlGrammarParser.CreateDefinitionContext> list = ctx.createDefinitions().createDefinition();
+        String[] attrList = new String[list.size()];
+        Type[] typeList = new Type[attrList.length];
+        for (int index = 0; index < attrList.length; index++) {
+            OddlGrammarParser.CreateDefinitionContext current = list.get(index);
+            attrList[index] = current.ID().getText();
+            switch (current.dataType().children.get(0).getText()) {
+                case "CHAR":
+                    typeList[index] = new Type(TypeEnum.CHAR_TYPE);
+                    typeList[index].setSize(Integer.parseInt(current.dataType().children.get(2).getText()));
+                    break;
+
+                case "INT":
+                    typeList[index] = new Type(TypeEnum.INT_TYPE);
+                    break;
+
+                case "LONG":
+                    typeList[index] = new Type(TypeEnum.LONG_TYPE);
+                    break;
+
+                case "FLOAT":
+                    typeList[index] = new Type(TypeEnum.FLOAT_TYPE);
+                    break;
+
+                case "BOOLEAN":
+                    typeList[index] = new Type(TypeEnum.BOOLEAN_TYPE);
+                    break;
+            }
+        }
+
+        catalog.addSourceClass(className, attrList, typeList);
+    }
+
+    @Override
+    public void enterCreateDeputyClass(OddlGrammarParser.CreateDeputyClassContext ctx) {
+        current = null;
+        treeNodeStack = new Stack<>();
+        rootNodeStack = new Stack<>();
+    }
+
+    @Override
+    public void exitCreateDeputyClass(OddlGrammarParser.CreateDeputyClassContext ctx) {
+
+        String sClassName = ctx.sClassName().getText();
+        if (catalog.getClassId(sClassName) == null) throw new IllegalArgumentException("Source Class doesn't exist.");
+        Catalog.AttrTableTuple[] sourceClassTypeList = catalog.getClassTypeList(catalog.getClassId(sClassName));
+        HashMap<String, Type> name2type = new HashMap<>();
+        for (Catalog.AttrTableTuple tuple : sourceClassTypeList) {
+            name2type.put(tuple.getAttrName(), tuple.getType());
+        }
+        String className = ctx.className().getText();
+        String[] switchExprs = new String[ctx.AS().size()];
+        String[] dAttr = new String[ctx.AS().size()];
+        for (int index = 0; index < switchExprs.length; index++) {
+            switchExprs[index] = ctx.expression(index).getText();
+            dAttr[index] = ctx.dAttr(index).getText();
+        }
+        String deputyRule = ctx.expression().get(ctx.expression().size() - 1).getText();
+    }
+
+    @Override
+    public void exitDropClass(OddlGrammarParser.DropClassContext ctx) {
+        String className = ctx.className().getText();
+        catalog.dropClass(className);
+    }
+
+    @Override
+    public void exitInsertIntoClass(OddlGrammarParser.InsertIntoClassContext ctx) {
+        String className = ctx.className().getText();
+        String[] valueList = new String[ctx.valueList().value().size()];
+        for (int index = 0; index < valueList.length; index++) {
+            valueList[index] = ctx.valueList().value(index).children.toString();
+            System.out.println(valueList[index]);
+        }
+        // value in String Format
+    }
+
+    @Override
+    public void exitDeleteFromClass(OddlGrammarParser.DeleteFromClassContext ctx) {
+        String className = ctx.className().getText();
+
+        // TODO: expression
+    }
+
+    @Override
+    public void exitSimpleQuery(OddlGrammarParser.SimpleQueryContext ctx) {
+        String className = ctx.className().getText();
+        String[] attrList = new String[ctx.attrList().attrName().size()];
+        for (int index = 0; index < attrList.length; index++) {
+            attrList[index] = ctx.attrList().attrName(index).getText();
+        }
+
+        // TODO: expression
+    }
+
+    @Override
+    public void exitCrossClassQuery(OddlGrammarParser.CrossClassQueryContext ctx) {
+        String className = ctx.className(0).getText();
+        String from_className = ctx.className(ctx.className().size() - 1).getText();
+        String attrName = ctx.attrName().getText();
+        String[] cross_classList = new String[ctx.className().size() - 2];
+        for (int index = 0; index < cross_classList.length; index++) {
+            cross_classList[index] = ctx.className(index + 1).getText();
+        }
+
+        // TODO: expression
+    }
+}
